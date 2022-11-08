9018ad8e38ab7de985cf51515214dbf53860304e
alexanastasgr <alexanastasgr@gmail.com>
2022-11-08 21:20:15 +0200

    add questions and handlers

--- src/Components/GameScreen.js
+++ src/Components/GameScreen.js
@@ -1,25 +1,44 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Questions } from "../Helpers/Questions";
 
-export default function GameScreen(){
-    return (
-        <div className='gamescreen'>
-            <div className='score'>
-                <h1>Score: <span id="score">0</span></h1>
-            </div>
-            <div className='question'>
-                <h1>THE QUESTION NAME</h1>
-                
-            </div>
+export default function GameScreen() {
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [selOption, setSelOption] = useState("");
+  const [score,setScore] = useState(0)
 
-            <span className="alert alert-error">Error Message</span>
+// check for correct answer
+  const submitAnswer = () => {
+    
 
-            <div className="answers">
-                <div className='answer'>ANSWER 1</div>
-                <div className='answer'>ANSWER 2</div>
-                <div className='answer'>ANSWER 3</div>
-                <div className='answer'>ANSWER 4</div>
-            </div>
-        </div>
-    )
+        if(selOption === Questions[currentQuestion].correct){
+           setScore(score + Questions[currentQuestion].points)
+           setCurrentQuestion(currentQuestion + 1)
+
+        }
+
+  }
+
+  return (
+    <div className="gamescreen">
+      <div className="score">
+        <h1>
+          Score: <span id="score">{score}</span>
+        </h1>
+      </div>
+      <div className="question">
+        <h1>💬 {Questions[currentQuestion].question}</h1>
+      </div>
+
+     
+
+      <div className="answers">
+        <div className="answer" onClick={()=>{setSelOption(1)}}>{Questions[currentQuestion].options[1]}</div>
+        <div className="answer" onClick={()=>{setSelOption(2)}}>{Questions[currentQuestion].options[2]}</div>
+        <div className="answer" onClick={()=>{setSelOption(3)}}>{Questions[currentQuestion].options[3]}</div>
+        <div className="answer" onClick={()=>{setSelOption(4)}}>{Questions[currentQuestion].options[4]}</div>
+      </div>
+
+      <div className="submitanswer" onClick={()=>{submitAnswer()}}>NEXT QUESTION</div>
+    </div>
+  );
 }
-
---
+++ src/Helpers/Questions.js
@@ -0,0 +1,38 @@
+export const Questions = [
+  {
+    question: "PHP is a",
+    points:10,
+    options: {
+      1: "Programming Language",
+      2: "Design pattern",
+      3: "Another answer",
+      4: "Opensource software",
+    },
+    correct: 1,
+  },
+
+
+  {
+    question: "What is Laravel ?",
+    points:5,
+    options: {
+      1: "JavaScript Library",
+      2: "OpenSource MVC framework",
+      3: "Modern image format",
+      4: "Linux Kernel",
+    },
+    correct: 2,
+  },
+
+  {
+    question: "To build a react app you must type: ",
+    points:5,
+    options: {
+      1: "npv --react build",
+      2: "npm run build",
+      3: "npx react -build",
+      4: "build react-app",
+    },
+    correct: 2,
+  },
+];
